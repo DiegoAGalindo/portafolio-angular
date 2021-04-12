20830ec5143eea91c4cb9db978f4b75eaa39251b
Diego Galindo <ingediegogalindo@gmail.com>
2021-04-12 17:16:17 -0500

    Fin de la sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,32 +1,69 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { resolve } from '../../../node_modules/@types/q';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
   productos : Producto[] = [];
+  productosFiltrado : Producto[] = [];
   cargando = true;
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
-
-
+    
    }
 
    private cargarProductos()
    {
-    this.http.get('https://angular-portafolio-23938-default-rtdb.firebaseio.com/productos_idx.json')
-        .subscribe( (resp:any) =>{
-//          console.log("productos");
-  //        console.log(resp); 
-  
+
+    return new Promise(  ( resolve, reject ) => {
+
+      this.http.get('https://angular-html-25cf9.firebaseio.com/productos_idx.json')
+          .subscribe( (resp: any) => {
             this.productos = resp;
-            // setTimeout(() => {
-            //   this.cargando = false;  
-            // }, 2000);
-            this.cargando = false;  
-        });
+            this.cargando = false;
+            resolve(null);
+          });
+
+    });
+    
+   }
+
+   public getProducto(id:string)
+   {
+    
+     return this.http.get(`https://angular-portafolio-23938-default-rtdb.firebaseio.com/productos/${id}.json`);
+   }
+
+   buscarProducto(termino:string){
+     if (this.productos.length===0)
+     {
+      console.log('if');
+       this.cargarProductos().then( ()=>{
+        this.filtrarProductos(termino);
+       } );
+       
+     }
+     else
+     {
+       console.log('else');
+      this.filtrarProductos(termino);
+     }     
+   }
+
+   private filtrarProductos(termino:string)
+   {
+     termino = termino.toLowerCase();
+     this.productosFiltrado = [];
+     this.productos.forEach(prod=>{
+       if (prod.categoria.toLowerCase().indexOf( termino ) >= 0 || prod.categoria.toLowerCase().indexOf(termino)>=0) {
+         this.productosFiltrado.push(prod);
+       }
+
+
+     } );
    }
 }
